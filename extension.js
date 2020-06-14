f0af493bbe78bc5778ff6d0245431363001328af
Daniel Shotonwa <danielshotonwa53@gmail.com>
2020-06-14 13:32:48 +0100

    get user text

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const editor = vscode.window.activeTextEditor;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,19 +9,14 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "autoenv" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('autoenv.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
-
+		const line = editor.selection.active.line;
+        const text = editor.document.lineAt(line).text;
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from autoenv!');
+		vscode.window.showInformationMessage(text.trim());
 	});
 
 	context.subscriptions.push(disposable);
